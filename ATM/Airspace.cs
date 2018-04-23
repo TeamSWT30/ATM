8ebf210f930b775ebc1a5175c41b3cdb5d317ad1
Tobias <tobiasniklassen@gmail.com>
2018-04-23 13:13:08 +0200

    added airspace and if sentence

---
+++ ATM/Airspace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class Airspace
+    {
+        public int SWCornerX { get; set; }
+        public int SWCornerY { get; set; }
+        public int NECornerX { get; set; }
+        public int NECornerY { get; set; }
+        public int lowerAlt { get; set; }
+        public int upperAlt { get; set; }
+    }
+}
--- ATM/TrackObjectification.cs
+++ ATM/TrackObjectification.cs
@@ -14,10 +14,16 @@ namespace ATM
         public List<Track> _tracks = new List<Track>();
         private ITrackRender _trackRender;
 
+        private Airspace _airspace = new Airspace()
+        {
+            SWCornerY = 10000, SWCornerX = 10000, NECornerX = 90000, NECornerY = 90000, lowerAlt = 500, upperAlt = 20000 
+        };
+
         public TrackObjectification(ITransponderReceiver transponderReceiver, ITransponderdataReader transponderdataReader,ITrackRender trackRender)
         {
             _trackRender = trackRender;
             _transponderdataReader = transponderdataReader;
+            
             transponderReceiver.TransponderDataReady += UpdateTrack;
         }
 
@@ -26,8 +32,13 @@ namespace ATM
             foreach (var data in args.TransponderData)
             {
                 var track = _transponderdataReader.ReadTrackData(data);
-                _tracks.Add(track);
-                _trackRender.RenderTrack(track);
+                if (track.X >= _airspace.SWCornerX && track.X <= _airspace.NECornerX &&
+                    track.Y >= _airspace.SWCornerY && track.Y <= _airspace.NECornerY &&
+                    track.Altitude >= _airspace.lowerAlt && track.Altitude <= _airspace.upperAlt)
+                {
+                    _tracks.Add(track);
+                    _trackRender.RenderTrack(track);
+                }
             }
             OnTrackChanged(new TracksChangedEventArgs {Tracks = _tracks});
         }
