db9bf59766c448fa610967edbc997e9be3ccd8ef
LeahPW <kolonP@live.dk>
2018-04-23 23:41:07 +0200

    fixed conflict

--- ATM/Conflict.cs
+++ ATM/Conflict.cs
@@ -10,7 +10,7 @@ namespace ATM
     public class Conflict : IConflict
     {
         private List<Track> ConflictingTracks;
-        event EventHandler<SeperationEventArgs> SeperationEvent;
+        public event EventHandler<SeperationEventArgs> SeperationEvent;
 
         public void CheckForConflicts(List<Track> Tracks)
         {
